1e16a0cfdbf9fd2fad1bf98bcfd0bba1ac4f7bfb
Tatiana Ciocca <18190041+Tacial@users.noreply.github.com>
2023-01-17 10:07:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -2,9 +2,8 @@
 const switcher = document.querySelector('.btn');
 switcher.addEventListener('click', function() {
     document.body.classList.toggle('dark-theme')
-
     var className = document.body.className;
-    
+   
     if(className == 'light-theme') {
         this.textContent = 'Dark';
     } else {
